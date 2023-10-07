839221e208fcca3816b955ab77d9366cd8bbb6ab
Zaraket007 <mohammadzaraket007@gmail.com>
2023-10-07 11:31:00 +0300

    first commit

---
+++ ContactsV3/app/src/main/java/com/example/contactsv3/ContactsInfo.java
@@ -0,0 +1,2 @@
+package com.example.contactsv3;public class ContactsInfo {
+}
---
+++ ContactsV3/app/src/main/java/com/example/contactsv3/ContactsViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.contactsv3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactsViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_view);
+    }
+}
